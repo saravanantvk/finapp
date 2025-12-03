a48a63b5386c5affd0d28a24cd00f3b5b1f27e7b
saravanantvk <saravanantvk@gmail.com>
2025-12-03 16:29:03 +0530

    Add console log message to index.js

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 // index.js
-module.exports = async (req, res) => {
-  if (req.method === 'GET') {
-    return res.json({ message: 'Hello from Appwrite Function API!' });
-  }
-  if (req.method === 'POST') {
-    const body = req.body && typeof req.body === 'string' ? JSON.parse(req.body) : req.body;
-    return res.json({ ok: true, received: body });
-  }
-  res.status(405).send('Method Not Allowed');
-};
+console.log("Hello from Node.js running inside GitHub repo!");
